98d42556004ae65db9db4abfdcc852f96621312d
MaeveHughes <maevehughes@gmail.com>
2022-03-28 20:13:01 +0000

    Adding adjust bag remove from bag views and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import (render, redirect, reverse,
+                              HttpResponse, get_object_or_404)
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
+
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag. It will take in both the request and an item id
-        which is the id of the product the user would like to add to their bag """
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -22,4 +27,47 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-    
+
+# adjust bag items
+# code taken from Boutique Ado project and adapted to this project
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get("quantity"))
+    bag = request.session.get("bag", {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.success(request,
+                         f"Updated {product.name} quantity to {bag[item_id]}")
+    else:
+        bag.pop(item_id)
+        messages.success(request, f"Removed {product.name} from your bag")
+
+    request.session["bag"] = bag
+
+    return redirect(reverse("view_bag"))
+
+# deleting bag items
+# code taken from Boutique Ado project and adapted to this project
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        bag = request.session.get("bag", {})
+        bag.pop(item_id)
+        messages.success(request, f"Removed {product.name} from your bag")
+
+        request.session["bag"] = bag
+
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f"Error removing item: {e}")
+        return HttpResponse(status=500)
